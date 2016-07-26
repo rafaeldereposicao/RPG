87266afff123c166edf4c0f2e3daba923d58ba30
infn3 <ejgsdc@gmail.com>
2016-07-26 20:17:53 -0300

    Update Arqueiro.java

--- src/Arqueiro.java
+++ src/Arqueiro.java
@@ -1,9 +1,9 @@
-
+//teste
 public class Arqueiro extends Classe {	
 	//AGIL,FORCA,INTEL
 	public Arqueiro(Personagem p){
 		
-		//ANOTAÇÕES IMPORTANTES**********************************
+		//ANOTAÃ‡Ã•ES IMPORTANTES**********************************
 		//ORDEM DE PREENCHIMENTO /AGILIDADE,FORCA,INTELIGENCIA
 		//ALVO | TRUE = ALVO UNICO | FALSE = TOOS OS ALVOS
 		//HABILIDADES DO ARQUEIRO
